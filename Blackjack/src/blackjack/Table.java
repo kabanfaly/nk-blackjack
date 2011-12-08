9d8ab7e73c28e572708edb93fc7118991ec7b123
nfalykaba@gmail.com <nfalykaba@gmail.com@66a3821d-a335-026f-c02c-e7f99aa42a27>
2011-12-08 10:34:58 +0000

    coding method melanger()

--- Blackjack/src/blackjack/Table.java
+++ Blackjack/src/blackjack/Table.java
@@ -33,6 +33,7 @@ public class Table {
     public void setCartes(ArrayList<Carte> cartes) {
         this.cartes = cartes;
     }
+
     /**
      * Accède au croupier
      * @return le croupier
@@ -64,28 +65,64 @@ public class Table {
     public void setJoueurs(ArrayList<Joueur> joueurs) {
         this.joueurs = joueurs;
     }
+
     /**
      * Ajoute un nouveau joueur
      * @param j 
      */
-    public void ajouterJoueur(Joueur j){
-        if(!joueurs.contains(j)){
+    public void ajouterJoueur(Joueur j) {
+        if (!joueurs.contains(j)) {
             joueurs.add(j);
         }
     }
+
     /**
      * Supprime un joueur
      * @param j 
      */
-    public void supprimerJoueur(Joueur j){
-        if(joueurs.contains(j)){
+    public void supprimerJoueur(Joueur j) {
+        if (joueurs.contains(j)) {
             joueurs.remove(j);
         }
     }
-    
-    public void supprimerCarte(Carte c){
-        if(cartes.contains(c)){
+
+    /**
+     * Supprime un carte du jeu de cartes
+     * @param c carte à supprimer
+     */
+    public void supprimerCarte(Carte c) {
+        if (cartes.contains(c)) {
             cartes.remove(c);
         }
+    }
+
+    /**
+     * Melange les carte du jeu
+     */
+    public void melanger() {       
+        //nombre de cartes
+        int n = cartes.size(); 
+        //effectue le melange 1000 fois
+        for(int i=0; i<1000; i++){
+            int position1 = Table.randomInt(n);
+            int position2 = Table.randomInt(n);
+            //echanger les positions
+            Carte tmp = cartes.get(position1);
+            cartes.set(position1, cartes.get(position2));
+            cartes.set(position2, tmp);
+        }
+        
+    }
+
+    
+    /**
+     * Renvoie un entier aleatoirement choisi entre 0 (compris) et max (non
+     * compris).
+     * @param max La borne sur l'entier pouvant etre generee.
+     * @return Un entier aleatoire.
+     */
+    public static int randomInt(int max) {
+        java.util.Random gen = new java.util.Random();
+        return gen.nextInt(max);
     }
 }
