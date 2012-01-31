1760335c831d5820c582da17c5bbe01f6239c032
nfalykaba@gmail.com <nfalykaba@gmail.com@66a3821d-a335-026f-c02c-e7f99aa42a27>
2012-01-31 16:38:15 +0000

    setting

---
+++ camion-wroom-wroom/src/camion/wroom/wroom/Camion.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package camion.wroom.wroom;
+
+/**
+ *
+ * @author BAILO
+ */
+public class Camion {   
+
+    public enum Type {
+
+        PETIT, MOYEN, GRAND, TRES_GRAND
+    }
+    private Type type;
+    
+}
---
+++ camion-wroom-wroom/src/camion/wroom/wroom/CamionWroomWroom.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package camion.wroom.wroom;
+
+/**
+ *
+ * @author BAILO
+ */
+public class CamionWroomWroom {
+
+    public static void afficherMenu(){
+        System.out.println("Afficher les tarifs et les capacités du camion");
+        System.out.println("Afficher le nombre de camions loués et le nombre de camions disponibles");
+        System.out.println("Evaluer les coûts de location");
+        System.out.println("Louer un camion");
+        System.out.println("Retourner un camion");
+        System.out.println("Quitter le programme");
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
